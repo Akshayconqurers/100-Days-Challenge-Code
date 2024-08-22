c28a9d3298be698012ac358a719d3239ce5c6c0f
Akshay Pratap Singh <117713892+Akshayconqurers@users.noreply.github.com>
2024-08-22 11:04:50 +0530

    Create SrtedMatrxTarget (Day 11).cpp

---
+++ SrtedMatrxTarget (Day 11).cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+bool searchinMatrix(int mat[4][4], int target){
+
+    for(int i=0; i<4; ++i){
+        for(int j =0 ; j < 4; ++j){
+            
+            if (mat[i][j] == target ){
+
+                cout << "Element found at position : "<< "(" << i << "," << j << ")" <<endl;
+                return true;
+            }
+        }
+    }
+    cout << "Element not found" <<endl;
+    return false;
+
+}
+
+int main(){
+
+    int mat[4][4] = {{1,3,4,6},
+                     {8,9,10,12},
+                     {14,15,17,29},
+                     {30,34,45,55}};
+
+    int target;
+    cout << "Enter the target :"<<endl;
+    cin >> target;
+
+    searchinMatrix(mat, target);
+
+    return 0;
+}
+
+
+
+// This is the brute force approach and two other approaches still can be used (Row & Column Wise and  Staircase approach )
+// Time Complexity -> O(n*m)
